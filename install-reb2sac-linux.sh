ae1fb91f462947a960ef9fa500f9f56890ac7cdf
cjmyers <myers@ece.utah.edu>
2007-02-22 16:00:42 +0000

    Changed REB2SAC_HOME to BIOSIM

--- install-reb2sac-linux.sh
+++ install-reb2sac-linux.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-env LDFLAGS="-L${REB2SAC_HOME}/linux/lib" CFLAGS="-I${REB2SAC_HOME}/include -O0 -g3 -DNAME_FOR_ID" ./configure  --prefix=${PWD}/.. && \
+env LDFLAGS="-L${BIOSIM}/lib" CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" ./configure  --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 clean && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install  
     
