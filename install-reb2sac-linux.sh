32a86f985f20f1c69fbcfb04dda10d1fb3a76b59
cjmyers <myers@ece.utah.edu>
2007-05-22 18:45:46 +0000

    Made reb2sac installer not recompile every time. Added docs/BioSim.html file.

--- install-reb2sac-linux.sh
+++ install-reb2sac-linux.sh
@@ -1,7 +1,13 @@
 #! /bin/sh
 
-env LDFLAGS="-L${BIOSIM}/lib" CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" ./configure  --prefix=${PWD}/.. && \
-env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 clean && \
-env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install  
+env LDFLAGS="-L${REB2SAC_HOME}/linux/lib"
+if [ -f "Makefile" ]; then
+env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+else
+CFLAGS="-I${REB2SAC_HOME}/include -O0 -g3 -DNAME_FOR_ID" ./configure
+--prefix=${PWD}/.. && \
+env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+fi
+
     
 
