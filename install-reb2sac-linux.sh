26cbfff3c376b8f8873e38a4475bb89afad8eeff
myers <>
2007-05-25 20:39:32 +0000

    fixed?

--- install-reb2sac-linux.sh
+++ install-reb2sac-linux.sh
@@ -1,12 +1,8 @@
 #! /bin/sh
 
-env LDFLAGS="-L${BIOSIM}/lib"
 if [ -f "Makefile" ]; then
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 else
-CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" ./configure --prefix=${PWD}/.. && \
+CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${BIOSIM}/lib" ./configure --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 fi
-
-    
-
