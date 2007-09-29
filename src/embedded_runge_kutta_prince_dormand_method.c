1b095f8d51a2832279c131939ba001a034fd744a
cjmyers <myers@ece.utah.edu>
2007-09-29 04:47:46 +0000

    Removed time=0 print

--- src/embedded_runge_kutta_prince_dormand_method.c
+++ src/embedded_runge_kutta_prince_dormand_method.c
@@ -254,7 +254,7 @@ static RET_VAL _RunSimulation( EMBEDDED_RUNGE_KUTTA_PRINCE_DORMAND_SIMULATION_RE
     printer = rec->printer;
     decider = rec->decider;
     while( !(decider->IsTerminationConditionMet( decider, NULL, time )) ) {
-      printf("Time = %.2f\n",time);
+      if (time > 0.0) printf("Time = %.2f\n",time);
       fflush(stdout);
         if( IS_FAILED( ( ret = _Print( rec, time ) ) ) ) {
             return ret;            
