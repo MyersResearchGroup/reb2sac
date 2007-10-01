63dea332945767186b81020b74478049e9aebe6b
myers <>
2007-10-01 15:49:18 +0000

    Files added for windows version.

---
+++ install-reb2sac-cygwin.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+if [ -f "Makefile" ]; then
+env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+else
+CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID -DWINDOWS" \
+LDFLAGS="-L${BIOSIM}/reb2sac/win/lib" ./configure --prefix=${PWD}/.. && \
+env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+fi
+
