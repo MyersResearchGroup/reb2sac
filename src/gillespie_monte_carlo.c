5720b268f5a0875f839575c5fca7b752141b52ad
cjmyers <myers@ece.utah.edu>
2008-03-12 20:24:49 +0000

    Fixed problem with last data item being printed multiple times when propensities go to 0.
    Fixed problem with time series match check when exporting dat.

--- src/gillespie_monte_carlo.c
+++ src/gillespie_monte_carlo.c
@@ -827,7 +827,7 @@ static RET_VAL _Print( GILLESPIE_MONTE_CARLO_RECORD *rec ) {
     REACTION *reaction = NULL;
     SIMULATION_PRINTER *printer = rec->printer;
 
-    while( nextPrintTime < time ) {
+    while(( nextPrintTime < time ) && ( nextPrintTime < rec->timeLimit )){
         if( IS_FAILED( ( ret = printer->PrintValues( printer, nextPrintTime ) ) ) ) {
             return ret;
         }
--- src/normal_waiting_time_monte_carlo.c
+++ src/normal_waiting_time_monte_carlo.c
@@ -770,7 +770,7 @@ static RET_VAL _Print( NORMAL_WAITING_TIME_MONTE_CARLO_RECORD *rec ) {
     REACTION *reaction = NULL;
     SIMULATION_PRINTER *printer = rec->printer;
 
-    while( nextPrintTime < time ) {
+    while(( nextPrintTime < time ) && ( nextPrintTime < rec->timeLimit )){
         if( IS_FAILED( ( ret = printer->PrintValues( printer, nextPrintTime ) ) ) ) {
             return ret;
         }
