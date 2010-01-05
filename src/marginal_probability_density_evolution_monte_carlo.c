8fbddd75796920b7044e3a8a687a3afb97eb2570
cmadsen <>
2010-01-05 22:58:17 +0000

    MP adaptive now allows for a number of desired events to be entered.

--- src/marginal_probability_density_evolution_monte_carlo.c
+++ src/marginal_probability_density_evolution_monte_carlo.c
@@ -740,6 +740,7 @@ static RET_VAL _RunSimulation(MPDE_MONTE_CARLO_RECORD *rec, BACK_END_PROCESSOR *
                 return ret;
             }
             n = ((1 / (rec->totalPropensities)) * timeStep);
+            printf("%d\n", n);
             //n = (n * rec->absoluteError);
             if ((n + time) > nextPrintTime) {
                 end = nextPrintTime;
