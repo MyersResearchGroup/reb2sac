7ad93e6dc9cad14f82a471c7028a8613c791536b
cjmyers <myers@ece.utah.edu>
2010-01-28 19:18:48 +0000

    Reordered gsl and gslcblas so gsl comes first.

--- install-reb2sac-cygwin.sh
+++ install-reb2sac-cygwin.sh
@@ -4,7 +4,7 @@ if [ -f "Makefile" ]; then
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 else
 CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID -DWINDOWS" \
-LDFLAGS="-L${BIOSIM}/reb2sac/win/lib" ./configure --prefix=${PWD}/.. && \
+LDFLAGS="-lm -L${BIOSIM}/reb2sac/win/lib" ./configure --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 fi
 
