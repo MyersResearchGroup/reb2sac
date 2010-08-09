8088458acb14365253ae0eee791b0523930537aa
thacker <>
2010-08-09 21:07:05 +0000

    I'll get the variables right eventually...

--- install-reb2sac-linux.sh
+++ install-reb2sac-linux.sh
@@ -3,6 +3,6 @@
 if [ -f "Makefile" ]; then
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 else
-CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${BIOSIM}/lib64" ./configure --prefix=${PWD}/.. && \
+CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${DYLD_LIBRARY_PATH}" ./configure --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 fi
