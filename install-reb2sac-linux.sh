172979bddf55e3078e4a1de34063c6a5418bf351
thacker <>
2010-08-11 21:01:49 +0000

    fixed machine dependent path

--- install-reb2sac-linux.sh
+++ install-reb2sac-linux.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
-
 if [ -f "Makefile" ]; then
+env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+else
+OS=$(uname -s)
+if [$OS -eq 'Linux']; then
+CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${LD_LIBRARY_PATH}" ./configure --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 else
 CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${DYLD_LIBRARY_PATH}" ./configure --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+fi
 fi
