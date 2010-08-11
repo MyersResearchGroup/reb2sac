584cf14b705a8b9ccbd101e5f3fc1fd35732b8bf
thacker <>
2010-08-11 21:50:20 +0000

    fixed path choice (again...)

--- install-reb2sac-linux.sh
+++ install-reb2sac-linux.sh
@@ -1,12 +1,16 @@
 #! /bin/sh
+OS=$(uname -s)
+
 if [ -f "Makefile" ]; then
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 else
-OS=$(uname -s)
-if [$OS -eq 'Linux']; then
+if test $OS = "Linux"
+then
+echo linux
 CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${LD_LIBRARY_PATH}" ./configure --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 else
+echo mac
 CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${DYLD_LIBRARY_PATH}" ./configure --prefix=${PWD}/.. && \
 env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 fi
