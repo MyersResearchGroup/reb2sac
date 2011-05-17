abe798826eedc051ca91292fd9fbeb1d5da4f7d6
cmadsen <>
2011-05-17 22:11:08 +0000

    Working on conservation analysis.

--- src/conservation_analysis.c
+++ src/conservation_analysis.c
@@ -328,7 +328,7 @@ gsl_vector *deprows(gsl_matrix *A)
 		gsl_vector_set(k, i, i+1-r); //row-rank
 		if(r<i+1)
 		{
-		    printf("Before\n");
+		    printf("%d \n", r);
 			int d = gsl_vector_get(k,i);
 			int s = gsl_vector_get(k,i-1);
 			printf("After\n");
