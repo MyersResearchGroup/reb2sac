eeaaab2ae5d34ed1e356495090635f43440c2141
cmadsen <>
2011-06-01 19:12:00 +0000

    Worked on conservation.

--- src/marginal_probability_density_evolution_monte_carlo.c
+++ src/marginal_probability_density_evolution_monte_carlo.c
@@ -760,19 +760,19 @@ static RET_VAL _RunSimulation(MPDE_MONTE_CARLO_RECORD *rec, BACK_END_PROCESSOR *
           speciesOrder[i] = speciesArray[i];
         }
         stoich_matrix = _GetStoichiometricMatrix(rec);
-        //printf("\nStoich = \n");
-	    //disp_mat(stoich_matrix);
-	    //for (i = 0; i < size; i ++ ) {
-	    //    printf("%d \n", GetSpeciesNodeID(speciesOrder[i]));
-	    //}
+        printf("\nStoich = \n");
+	    disp_mat(stoich_matrix);
+	    for (i = 0; i < size; i ++ ) {
+	        printf("%d \n", GetSpeciesNodeID(speciesOrder[i]));
+	    }
         L_matrix = conservation(stoich_matrix, speciesOrder);
-        //for (i = 0; i < size; i ++ ) {
-        //       printf("%d \n", GetSpeciesNodeID(speciesOrder[i]));
-        // }
+        for (i = 0; i < size; i ++ ) {
+               printf("%d \n", GetSpeciesNodeID(speciesOrder[i]));
+         }
         Lo_matrix = linkzero(L_matrix);
         G_matrix = gamma_matrix(Lo_matrix);
-        //printf("\nG = \n");
-	    //disp_mat(G_matrix);
+        printf("\nG = \n");
+	    disp_mat(G_matrix);
     }
 
     meanPrinter = rec->meanPrinter;
