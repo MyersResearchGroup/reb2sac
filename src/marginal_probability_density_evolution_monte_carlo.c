805e9a8af9d2aa552bec59ddad9eca4c00cb1a12
cmadsen <>
2012-03-15 21:39:58 +0000

    Added reaction rate equations to the statistics file.

--- src/marginal_probability_density_evolution_monte_carlo.c
+++ src/marginal_probability_density_evolution_monte_carlo.c
@@ -1898,7 +1898,7 @@ static RET_VAL _PrintStatistics(MPDE_MONTE_CARLO_RECORD *rec, FILE *file) {
 	for (i = 0; i < symbolArray; i++) {
 		symbol = symbolArray[i];
 		if (IsRealValueSymbol(symbol)) {
-			fprintf( file, "%s = %f" NEW_LINE, *GetSymbolID(symbol), GetRealValueInSymbol(symbol));
+			//fprintf( file, "%s = %f" NEW_LINE, *GetSymbolID(symbol), GetRealValueInSymbol(symbol));
 		}
 	}
 	fprintf( file, NEW_LINE);
