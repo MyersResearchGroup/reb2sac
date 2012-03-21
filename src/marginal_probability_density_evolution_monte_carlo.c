2069f77237790d15cb81c4bf65cf171bcd5a26c8
cmadsen <>
2012-03-21 06:03:31 +0000

    Working on MP-adaptive.

--- src/marginal_probability_density_evolution_monte_carlo.c
+++ src/marginal_probability_density_evolution_monte_carlo.c
@@ -1323,7 +1323,7 @@ static RET_VAL _RunSimulation(MPDE_MONTE_CARLO_RECORD *rec, BACK_END_PROCESSOR *
                 if (IS_REAL_EQUAL(smallProp, 0.0)) {
                 	n = timeStep;
                 } else {
-                	n = (timeStep / smallProp);
+                	n = (timeStep / (smallProp*100));
                 }
                 printf("Small-Prop = %f; Events = %f; Incre-Size = %f", smallProp, timeStep, n);
                 end = n + rec->time;
