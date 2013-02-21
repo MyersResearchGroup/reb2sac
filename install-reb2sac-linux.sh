7e25eec571876e4b23eca96e6b14e7a57809a980
myers <>
2013-02-21 21:26:58 +0000

    Add support to target Lion for MacOS.

--- install-reb2sac-linux.sh
+++ install-reb2sac-linux.sh
@@ -1,18 +1,22 @@
 #! /bin/sh
-OS=$(uname -m)
-
+ARCH=$(uname -m)
+OS=$(uname)
 if [ -f "Makefile" ]; then
-env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+    env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
 else
-if test $OS = "x86_64"
-then
-echo x86_64
-CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${BIOSIM}/lib64 -lm" ./configure --prefix=${PWD}/.. && \
-env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
-echo /linux
-else
-echo i686
-CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${BIOSIM}/lib -lm" ./configure --prefix=${PWD}/.. && \
-env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
-fi
+    if test $OS = "Darwin" 
+    then
+	CFLAGS="-I${BIOSIM}/include -O0 -g3 -mmacosx-version-min=10.7 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX10.7.sdk -arch x86_64 -DNAME_FOR_ID" \
+	    LDFLAGS="-L${BIOSIM}/lib64 -lm" ./configure --prefix=${PWD}/.. && \
+	    env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+    else 
+	if test $ARCH = "x86_64" 
+	then
+	    CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${BIOSIM}/lib64 -lm" ./configure --prefix=${PWD}/.. && \
+		env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+	else
+	    CFLAGS="-I${BIOSIM}/include -O0 -g3 -DNAME_FOR_ID" LDFLAGS="-L${BIOSIM}/lib -lm" ./configure --prefix=${PWD}/.. && \
+		env WANT_AUTOCONF_2_5="1" WANT_AUTOMAKE_1_6="1" make -k -j1 install
+	fi
+    fi
 fi
